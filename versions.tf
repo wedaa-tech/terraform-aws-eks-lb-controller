44286b67d4ab6cea3a272cf6f06099400e60a0d6
rakesh-wedaa <rakesh.c@wedaa.tech>
2025-07-24 22:16:41 -0500

    updated the helm version

--- versions.tf
+++ versions.tf
@@ -1,9 +1,13 @@
 terraform {
-  # required_version = ">= 1.2.0"
+  required_version = ">= 1.8.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.6"
     }
   }
 }
